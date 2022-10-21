83bfc33a90f3451f4b71fe93625a5fb11fd5d014
Mike327327 <mike327@seznam.cz>
2022-10-21 18:51:02 +0000

    train_vits_tts_d_vector.py added - support for training with d-vecs

---
+++ recipes/multilingual/vits_tts/train_vits_tts_d_vector.py
@@ -0,0 +1,145 @@
+import os
+from glob import glob
+
+from trainer import Trainer, TrainerArgs
+
+from TTS.config.shared_configs import BaseAudioConfig
+from TTS.tts.configs.shared_configs import BaseDatasetConfig
+from TTS.tts.configs.vits_config import VitsConfig
+from TTS.tts.datasets import load_tts_samples
+from TTS.tts.models.vits import CharactersConfig, Vits, VitsArgs
+from TTS.tts.utils.languages import LanguageManager
+from TTS.tts.utils.speakers import SpeakerManager
+from TTS.tts.utils.text.tokenizer import TTSTokenizer
+from TTS.utils.audio import AudioProcessor
+
+output_path = os.path.dirname(os.path.abspath(__file__))
+dataset_config = BaseDatasetConfig(
+    name="mailabs", meta_file_train="metadata.txt", language="cs-cz", path="/content/snemovna/"
+)
+
+def snemovna_formatter(root_path, manifest_file, **kwargs): 
+    """Assumes each line as ```<filename>|<transcription>|<transcription>```
+    """
+    txt_file = os.path.join(root_path, manifest_file)
+    items = []
+    speaker_name = ""
+    with open(txt_file, "r", encoding="utf-8") as ttf:
+        for line in ttf:
+            cols = line.split("|")
+            wav_file = os.path.join(root_path, "wavs", cols[0]) + ".wav"
+            text = cols[1]
+            speaker_name = cols[0][:7]
+            items.append({"text":text, "audio_file":wav_file, "speaker_name":speaker_name, "root_path": root_path})
+    return items
+
+audio_config = BaseAudioConfig(
+    sample_rate=16000,
+    win_length=1024,
+    hop_length=256,
+    num_mels=80,
+    preemphasis=0.0,
+    ref_level_db=20,
+    log_func="np.log",
+    do_trim_silence=False,
+    trim_db=23.0,
+    mel_fmin=0,
+    mel_fmax=None,
+    spec_gain=1.0,
+    signal_norm=True,
+    do_amp_to_db_linear=False,
+    resample=False,
+)
+
+vitsArgs = VitsArgs(
+    use_language_embedding=False,
+    use_speaker_embedding=False,
+    use_sdp=False,
+    use_d_vector_file=True,
+    d_vector_file="/content/drive/MyDrive/YourTTS/embeddings_d_vectors.pkl",
+    d_vector_dim= 512,
+)
+
+config = VitsConfig(
+    model_args=vitsArgs,
+    audio=audio_config,
+    run_name="vits_snemovna",
+    use_speaker_embedding=False,
+    batch_size=16,
+    eval_batch_size=8,
+    batch_group_size=0,
+    num_loader_workers=4,
+    num_eval_loader_workers=4,
+    run_eval=True,
+    test_delay_epochs=-1,
+    epochs=1000,
+    text_cleaner="multilingual_cleaners",
+    use_phonemes=False,
+    phoneme_language="cs-cz",
+    phoneme_cache_path=os.path.join(output_path, "phoneme_cache"),
+    compute_input_seq_cache=True,
+    print_step=25,
+    use_language_weighted_sampler=True,
+    print_eval=False,
+    mixed_precision=False,
+    # sort_by_audio_len=True,
+    min_audio_len=32 * 256 * 4,
+    max_audio_len=160000,
+    output_path=output_path,
+    datasets=dataset_config,
+    characters=CharactersConfig(
+        characters_class="TTS.tts.models.vits.VitsCharacters",
+        pad="<PAD>",
+        eos="<EOS>",
+        bos="<BOS>",
+        blank="<BLNK>",
+        characters="!¡'(),-.:;¿?abcdefghijklmnopqrstuvwxyzěščřňůťďžýµßàáâäåæçèéêëìíîïñòóôöùúûüąćęłńœśşźżƒабвгдежзийклмнопрстуфхцчшщъыьэюяёєіїґӧ «°±µ»$%&‘’‚“`”„",
+        punctuations="!¡'(),-.:;¿? ",
+        phonemes=None,
+    ),
+    test_sentences=[
+        [
+            "Ahoj",
+            "spk_000",
+            None,
+            "cs_CZ",
+        ]
+    ],
+    add_blank=False,
+
+    use_d_vector_file=True,
+    d_vector_file="/content/drive/MyDrive/YourTTS/embeddings_d_vectors.pkl,",
+    d_vector_dim=512,
+)
+
+# init audio processor
+ap = AudioProcessor(**config.audio.to_dict())
+
+# load training samples
+train_samples, eval_samples = load_tts_samples(
+    dataset_config,
+    eval_split=True,
+    eval_split_max_size=config.eval_split_max_size,
+    eval_split_size=config.eval_split_size,
+    formatter=snemovna_formatter,
+)
+
+# init speaker manager for multi-speaker training
+# it maps speaker-id to speaker-name in the model and data-loader
+speaker_manager = SpeakerManager()
+speaker_manager.set_ids_from_data(train_samples + eval_samples, parse_key="speaker_name")
+config.model_args.num_speakers = speaker_manager.num_speakers
+
+# INITIALIZE THE TOKENIZER
+# Tokenizer is used to convert text to sequences of token IDs.
+# config is updated with the default characters if not defined in the config.
+tokenizer, config = TTSTokenizer.init_from_config(config)
+
+# init model
+model = Vits(config, ap, tokenizer, speaker_manager)
+
+# init the trainer and 🚀
+trainer = Trainer(
+    TrainerArgs(), config, output_path, model=model, train_samples=train_samples, eval_samples=eval_samples
+)
+trainer.fit()
